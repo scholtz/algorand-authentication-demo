d77dbcbd2ee58fa3afddc1a200845e3ed7688005
Ludovit Scholtz <ludko2@gmail.com>
2023-10-20 09:40:24 +0200

    test getTransactionParams

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,90 @@
 <script setup lang="ts">
 import { onMounted, reactive, ref } from 'vue'
 import algosdk from 'algosdk'
-import { useToast } from 'primevue/usetoast'
-import fetch from 'cross-fetch'
+// import { useToast } from 'primevue/usetoast'
 
-const fetch2 = window.fetch.bind(window)
+// const toast = useToast()
 
-const toast = useToast()
+// import { AlgorandAuthentication } from 'algorand-authentication-component-vue'
+// import type {
+//   IAlgorandAuthenticationStore,
+//   INotification
+// } from 'algorand-authentication-component-vue'
+// const defaultAuthState: IAlgorandAuthenticationStore = {
+//   isAuthenticated: false,
+//   arc14Header: '',
+//   wallet: '',
+//   account: '',
+//   count: 0,
+//   arc76email: ''
+// }
+// const authState = reactive({
+//   isAuthenticated: defaultAuthState.isAuthenticated,
+//   arc14Header: defaultAuthState.arc14Header,
+//   wallet: defaultAuthState.wallet,
+//   account: defaultAuthState.account,
+//   count: defaultAuthState.count,
+//   arc76email: defaultAuthState.arc76email
+// })
 
-const show = () => {
-  console.log('show')
-  toast.add({ severity: 'info', summary: 'Info', detail: 'Message Content', life: 3000 })
-}
-import { AlgorandAuthentication } from 'algorand-authentication-component-vue'
-import type {
-  IAlgorandAuthenticationStore,
-  INotification
-} from 'algorand-authentication-component-vue'
-const defaultAuthState: IAlgorandAuthenticationStore = {
-  isAuthenticated: false,
-  arc14Header: '',
-  wallet: '',
-  account: '',
-  count: 0,
-  arc76email: ''
-}
-const authState = reactive({
-  isAuthenticated: defaultAuthState.isAuthenticated,
-  arc14Header: defaultAuthState.arc14Header,
-  wallet: defaultAuthState.wallet,
-  account: defaultAuthState.account,
-  count: defaultAuthState.count,
-  arc76email: defaultAuthState.arc76email
-})
+// function onStateChange(e: IAlgorandAuthenticationStore) {
+//   console.log('onStateChange', e)
+//   authState.isAuthenticated = e.isAuthenticated
+//   authState.arc14Header = e.arc14Header
+//   authState.wallet = e.wallet
+//   authState.account = e.account
+//   authState.count = e.count
+//   authState.arc76email = e.arc76email
+// }
+// function onNotification(e: INotification) {
+//   try {
+//     if (e.severity == 'error') {
+//       console.error(e.message)
+//     } else {
+//       console.log('onNotification', e)
+//     }
+//     const ret = toast.add({
+//       severity: e.severity,
+//       detail: e.message,
+//       life: 5000
+//     })
+//     console.log('ret', ret)
+//   } catch (e: any) {
+//     console.error(e.message)
+//   }
+// }
+// console.log('AlgorandAuthentication', AlgorandAuthentication)
+// const authComponent = ref<InstanceType<typeof AlgorandAuthentication>>()
 
-function onStateChange(e: IAlgorandAuthenticationStore) {
-  console.log('onStateChange', e)
-  authState.isAuthenticated = e.isAuthenticated
-  authState.arc14Header = e.arc14Header
-  authState.wallet = e.wallet
-  authState.account = e.account
-  authState.count = e.count
-  authState.arc76email = e.arc76email
-}
-function onNotification(e: INotification) {
-  try {
-    if (e.severity == 'error') {
-      console.error(e.message)
-    } else {
-      console.log('onNotification', e)
-    }
-    const ret = toast.add({
-      severity: e.severity,
-      detail: e.message,
-      life: 5000
-    })
-    console.log('ret', ret)
-  } catch (e: any) {
-    console.error(e.message)
-  }
-}
-console.log('AlgorandAuthentication', AlgorandAuthentication)
-const authComponent = ref<InstanceType<typeof AlgorandAuthentication>>()
+// async function signTx() {
+//   if (!authComponent?.value) return
 
-async function signTx() {
-  if (!authComponent?.value) return
+//   const tx = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+//     amount: 0,
+//     from: authState.account,
+//     suggestedParams: {
+//       fee: 0,
+//       firstRound: 32961555,
+//       genesisHash: 'wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=',
+//       genesisID: 'mainnet-v1.0',
+//       lastRound: 32962555,
+//       flatFee: true
+//     },
+//     to: authState.account
+//   })
 
-  const tx = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
-    amount: 0,
-    from: authState.account,
-    suggestedParams: {
-      fee: 0,
-      firstRound: 32961555,
-      genesisHash: 'wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=',
-      genesisID: 'mainnet-v1.0',
-      lastRound: 32962555,
-      flatFee: true
-    },
-    to: authState.account
-  })
-
-  const signed = await authComponent.value.sign([algosdk.encodeUnsignedTransaction(tx)])
-  console.log('signed', signed)
-}
-async function logout() {
-  if (!authComponent?.value) return
-  const logout = await authComponent.value.logout()
-  console.log('logout', logout)
-}
+//   const signed = await authComponent.value.sign([algosdk.encodeUnsignedTransaction(tx)])
+//   console.log('signed', signed)
+// }
+// async function logout() {
+//   if (!authComponent?.value) return
+//   const logout = await authComponent.value.logout()
+//   console.log('logout', logout)
+// }
 
 const client = new algosdk.Algodv2('', 'https://mainnet-api.algonode.cloud', 443)
 
 onMounted(async () => {
-  const fetchData = await fetch2('https://mainnet-api.algonode.cloud/v2/transactions/params')
-  console.log('fetchData', fetchData)
   const params = await client.getTransactionParams().do()
   console.log('params', params)
 })
@@ -101,7 +92,7 @@ onMounted(async () => {
 <template>
   <div class="flex flex-column justify-content-center min-h-full p-0 m-0">
     <Toast />
-    <Suspense>
+    <!-- <Suspense>
       <AlgorandAuthentication
         @onStateChange="onStateChange"
         @onNotification="onNotification"
@@ -116,7 +107,7 @@ onMounted(async () => {
         <button :onclick="signTx">Sign</button>
         <button :onclick="logout">Logout</button>
       </AlgorandAuthentication>
-    </Suspense>
+    </Suspense> -->
   </div>
 </template>
 <style>
