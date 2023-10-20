b2ce3f0dc19220def4e256250a4b0310e57306bf
Ludovit Scholtz <ludko2@gmail.com>
2023-10-20 11:45:56 +0200

    demo auth component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,121 @@
 <script setup lang="ts">
-import { reactive, onMounted } from 'vue'
+import { reactive, ref } from 'vue'
 import algosdk from 'algosdk'
 
-const status = reactive({
-  err: {
-    message: 'no error'
-  },
-  data: {}
+import { useToast } from 'primevue/usetoast'
+const toast = useToast()
+
+const show = () => {
+  console.log('show')
+  toast.add({ severity: 'info', summary: 'Info', detail: 'Message Content', life: 3000 })
+}
+import { AlgorandAuthentication } from 'algorand-authentication-component-vue'
+import type {
+  IAlgorandAuthenticationStore,
+  INotification
+} from 'algorand-authentication-component-vue'
+const defaultAuthState: IAlgorandAuthenticationStore = {
+  isAuthenticated: false,
+  arc14Header: '',
+  wallet: '',
+  account: '',
+  count: 0,
+  arc76email: ''
+}
+const authState = reactive({
+  isAuthenticated: defaultAuthState.isAuthenticated,
+  arc14Header: defaultAuthState.arc14Header,
+  wallet: defaultAuthState.wallet,
+  account: defaultAuthState.account,
+  count: defaultAuthState.count,
+  arc76email: defaultAuthState.arc76email
 })
 
-function testGetParams() {
+function onStateChange(e: IAlgorandAuthenticationStore) {
+  console.log('onStateChange', e)
+  authState.isAuthenticated = e.isAuthenticated
+  authState.arc14Header = e.arc14Header
+  authState.wallet = e.wallet
+  authState.account = e.account
+  authState.count = e.count
+  authState.arc76email = e.arc76email
+}
+function onNotification(e: INotification) {
   try {
-    const client = new algosdk.Algodv2('', 'https://mainnet-api.algonode.cloud', 443)
-    client
-      .getTransactionParams()
-      .do()
-      .then((params) => {
-        status.data = params
-        console.log('params', params)
-      })
-      .catch((reason) => {
-        status.err = reason
-      })
+    if (e.severity == 'error') {
+      console.error(e.message)
+    } else {
+      console.log('onNotification', e)
+    }
+    const ret = toast.add({
+      severity: e.severity,
+      detail: e.message,
+      life: 5000
+    })
+    console.log('ret', ret)
   } catch (e: any) {
-    status.err = e
+    console.error(e.message)
   }
 }
-</script>
+console.log('AlgorandAuthentication', AlgorandAuthentication)
+const authComponent = ref<InstanceType<typeof AlgorandAuthentication>>()
 
+async function signTx() {
+  if (!authComponent?.value) return
+
+  const tx = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+    amount: 0,
+    from: authState.account,
+    suggestedParams: {
+      fee: 0,
+      firstRound: 32961555,
+      genesisHash: 'wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=',
+      genesisID: 'mainnet-v1.0',
+      lastRound: 32962555,
+      flatFee: true
+    },
+    to: authState.account
+  })
+
+  const signed = await authComponent.value.sign([algosdk.encodeUnsignedTransaction(tx)])
+  console.log('signed', signed)
+}
+async function logout() {
+  if (!authComponent?.value) return
+  const logout = await authComponent.value.logout()
+  console.log('logout', logout)
+}
+
+const client = new algosdk.Algodv2('', 'https://mainnet-api.algonode.cloud', 443)
+</script>
 <template>
-  <button @click="testGetParams">Push</button>
-  <div>Error: {{ status.err.message }}</div>
-  <div>Data : {{ status.data }}</div>
+  <div class="flex flex-column justify-content-center min-h-full p-0 m-0">
+    <Toast />
+    <Suspense>
+      <AlgorandAuthentication
+        @onStateChange="onStateChange"
+        @onNotification="onNotification"
+        ref="authComponent"
+        :wallets="['pera', 'exodus', 'defly', 'myalgo', 'mnemonic']"
+        useDemoMnemonics="novel consider desert ribbon cage first audit couple discover seed text guard crater exchange roof stable march tortoise hockey magic dawn jacket cricket ability bright"
+        :algodClient="client"
+      >
+        <h1>Authenticated Content {{ authState.count }}</h1>
+        <div>
+          Account: {{ authState.arc76email }} {{ authState.wallet }} / {{ authState.account }}
+        </div>
+        <button :onclick="signTx">Sign</button>
+        <button :onclick="logout">Logout</button>
+      </AlgorandAuthentication>
+    </Suspense>
+  </div>
 </template>
+<style>
+.wallets-page {
+  background-size: cover;
+  background-image: url('/auth-cover.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
-// import 'algorand-authentication-component-vue/dist/style.css'
-// import 'primeflex/primeflex.scss'
-// import './assets/main.css'
-// import 'primevue/resources/themes/lara-light-indigo/theme.css'
-// import 'primevue/resources/primevue.min.css' /* Deprecated */
+import 'algorand-authentication-component-vue/dist/style.css'
+import 'primeflex/primeflex.scss'
+import './assets/main.css'
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
 
 import { createApp } from 'vue'
 
 import App from './App.vue'
 
-// import PrimeVue from 'primevue/config'
-// import Toast from 'primevue/toast'
-// import Button from 'primevue/button'
-// import ToastService from 'primevue/toastservice'
+import PrimeVue from 'primevue/config'
+import Toast from 'primevue/toast'
+import Button from 'primevue/button'
+import ToastService from 'primevue/toastservice'
 
 export const app = createApp(App)
-// app.use(PrimeVue, { ripple: true })
-// app.use(ToastService)
+app.use(PrimeVue, { ripple: true })
+app.use(ToastService)
 
-// app.component('Button', Button)
-// app.component('Toast', Toast)
+app.component('Button', Button)
+app.component('Toast', Toast)
 
-//app.use(a)
 app.mount('#app')
